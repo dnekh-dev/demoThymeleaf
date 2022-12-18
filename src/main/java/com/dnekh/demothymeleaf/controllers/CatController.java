1ba80776276603a8b398480f10ed563931632993
d_nekh <dnekh.dev@gmail.com>
2022-12-18 18:54:32 +0900

    Here were added model's class CatModel and created tag Table in index file

--- src/main/java/com/dnekh/demothymeleaf/controllers/CatController.java
+++ src/main/java/com/dnekh/demothymeleaf/controllers/CatController.java
@@ -1,8 +1,11 @@
 package com.dnekh.demothymeleaf.controllers;
 
+import com.dnekh.demothymeleaf.models.CatModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class CatController {
@@ -10,6 +13,11 @@ public class CatController {
     @GetMapping("/home")
     public String getCat(Model model) {
         model.addAttribute("hello", "Hello Kitty Super Mors!");
+        model.addAttribute("cat", Arrays.asList(
+                new CatModel("Mors", 7),
+                new CatModel("Boris", 5),
+                new CatModel("Lucy", 6)
+        ));
         return "index";
     }
 }
